04bd2f0ece2901d3478656fa317b4f774e63c742
EdsonJSF <edsonjsf1990@gmail.com>
2023-07-06 08:58:32 -0400

    Rol validation

--- routes/users.route.js
+++ routes/users.route.js
@@ -1,5 +1,6 @@
 const { Router } = require("express");
 const { check } = require("express-validator");
+const Role = require("../models/role");
 
 const {
   getUsers,
@@ -19,7 +20,12 @@ router.post(
     check("name", "Invalid name").not().isEmpty(),
     check("email", "Invalid email").isEmail(),
     check("password", "Invalid password").isLength({ min: 6 }),
-    check("rol", "Invalid rol").isIn(["ADMIN_ROLE", "USER_ROLE"]),
+    check("rol").custom(async (rol = "") => {
+      const role = await Role.findOne({ rol });
+      if (!role) {
+        throw new Error(`rol ${rol} Invalid`);
+      }
+    }),
     validateFields,
   ],
   postUser
