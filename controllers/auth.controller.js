29302e3fd7b5e39207c66bde1acedd4ed0b121d4
EdsonJSF <edsonjsf1990@gmail.com>
2023-07-17 21:36:18 -0400

    Feat: Login user - Compare email, status and password

--- controllers/auth.controller.js
+++ controllers/auth.controller.js
@@ -1,11 +1,54 @@
 const { response } = require("express");
+const bcryptjs = require("bcryptjs");
 
-const authLogin = (req, res = response) => {
-  const { ...body } = req.body;
-  res.json({
-    msg: "Login ok",
-    data: body,
-  });
+const User = require("../models/user.model");
+
+const authLogin = async (req, res = response) => {
+  const { email, password } = req.body;
+
+  try {
+    // Verify email
+    const user = await User.findOne({ email });
+    if (!user) {
+      return res.status(400).json({
+        msg: "Email / Password Invalid - email",
+        errors: [],
+      });
+    }
+
+    // User is active ?
+    if (!user.state) {
+      return res.status(400).json({
+        msg: "Email / Password Invalid - state",
+        errors: [],
+      });
+    }
+
+    // Verify password
+    const validPassword = bcryptjs.compareSync(password, user.password);
+    if (!validPassword) {
+      return res.status(400).json({
+        msg: "Email / Password Invalid - pass",
+        errors: [],
+      });
+    }
+
+    // Generate JWT
+
+    res.json({
+      msg: "Login ok",
+      data: {
+        email,
+      },
+    });
+  } catch (error) {
+    console.log(error);
+
+    res.status(500).json({
+      msg: "An unspected error",
+      errors: [],
+    });
+  }
 };
 
 module.exports = {
