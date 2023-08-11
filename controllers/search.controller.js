b067b384a08be5a27e05ed93eefab2d175ff10f8
EdsonJSF <edsonjsf1990@gmail.com>
2023-08-11 09:42:53 -0400

    Search DB controller

--- controllers/search.controller.js
+++ controllers/search.controller.js
@@ -1,11 +1,58 @@
 const { response, request } = require("express");
+const { Category, Product, Role, User } = require("../models");
 
-const search = async (req = request, res = response) => {
-  const { collection, term } = req.params;
+const { ObjectId } = require("mongoose").Types;
+
+const collections = ["categories", "products", "roles", "users"];
+
+const search = (req = request, res = response) => {
+  let { collection, term } = req.params;
+  collection = collection.toLowerCase();
+  term = term.toUpperCase();
+
+  if (!collections.includes(collection)) {
+    res.status(400).json({
+      msg: `Collection ${collection} is not allowed`,
+      errors: [],
+    });
+  }
+
+  switch (collection) {
+    case "categories":
+      doSearch(Category, term, res);
+      break;
+    case "products":
+      doSearch(Product, term, res);
+      break;
+    case "roles":
+      doSearch(Role, term, res);
+      break;
+    case "users":
+      doSearch(User, term, res);
+      break;
+
+    default:
+      res.status(500).json({
+        msg: "collection error, contact the administrator",
+        errors: [],
+      });
+      break;
+  }
+};
+
+const doSearch = async (model, term, res = response) => {
+  const mongoId = ObjectId.isValid(term);
+
+  let searchResult = [];
+  if (mongoId) {
+    searchResult = await model.findById(term);
+  } else {
+    searchResult = await model.findOne({ name: term });
+  }
 
   return res.json({
-    msg: `Searching ${term} => ${collection}`,
-    data: { collection, term },
+    msg: `Searching ${term} finished`,
+    data: searchResult ? [searchResult] : [],
   });
 };
 
