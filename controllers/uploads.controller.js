175d121e2adbb40147d65d5b0255d91596e295f9
EdsonJSF <edsonjsf1990@gmail.com>
2023-08-28 08:46:47 -0400

    Middleware validateFile

--- controllers/uploads.controller.js
+++ controllers/uploads.controller.js
@@ -3,27 +3,6 @@ const { uploadFile } = require("../helpers");
 const { User, Product } = require("../models");
 
 const loadFile = async (req, res = response) => {
-  if (!req.files || Object.keys(req.files).length === 0) {
-    return res.status(400).json({
-      msg: "No files were uploaded.",
-      errors: [],
-    });
-  }
-
-  if (!req.files.file) {
-    return res.status(400).json({
-      msg: "You have some request errors",
-      errors: [
-        {
-          type: "field",
-          msg: "Invalid file",
-          path: "file",
-          location: "body",
-        },
-      ],
-    });
-  }
-
   uploadFile(req.files)
     .then((resp) => {
       const { file } = req.files;
--- routes/uploads.route.js
+++ routes/uploads.route.js
@@ -5,15 +5,20 @@ const { check } = require("express-validator");
 const { loadFile, updateFile } = require("../controllers/uploads.controller");
 
 // Middlewares
-const { validateFields, validateCollections } = require("../middlewares");
+const {
+  validateFields,
+  validateCollections,
+  validateFile,
+} = require("../middlewares");
 
 const router = Router();
 
-router.post("/", [], loadFile);
+router.post("/", [validateFile], loadFile);
 
 router.put(
   "/:collection/:id",
   [
+    validateFile,
     check("id", "Invalid id").isMongoId(),
     check("collection").custom((c) =>
       validateCollections(c, ["users", "products"])
